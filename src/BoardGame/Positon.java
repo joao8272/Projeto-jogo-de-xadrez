481f4a63b6392c74e032d4f0dcc7d7a4a6074adf
Joao Pedro <pedro.arofilgueira@gmail.com>
2026-04-01 09:30:35 -0300

    add

---
+++ src/BoardGame/Positon.java
@@ -0,0 +1,29 @@
+package BoardGame;
+
+public class Positon {
+    private int row;
+    private int column;
+
+    public Positon(){
+    }
+    public Positon(int row, int column){
+        this.row = row;
+        this.column = column;
+    }
+    public int getRow() {
+        return row;
+    }
+    public void setRow(int row) {
+        this.row = row;
+    }
+    public int getColumn() {
+        return column;
+    }
+    public void setColumn(int column) {
+        this.column = column;
+    }
+    @Override
+    public String toString() {
+        return row + ", " + column;
+    }
+}
